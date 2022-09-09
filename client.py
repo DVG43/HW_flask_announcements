f4d4d543f1ca2940b0a1bd1a6878dd8db2441527
Dmitrii <dvgaluta@gmail.com>
2022-09-09 23:28:54 +0300

    commit_7

--- client.py
+++ client.py
@@ -6,6 +6,17 @@ response = requests.get(HOST)
 print(response.status_code)
 print(response.text)
 
-response = requests.get(f'{HOST}/test/')
+# response = requests.post(f'{HOST}/announsments/', json={'headline': 'car',
+#                                                        'description': 'new BMW',
+#                                                        'owner': 'Sasha',
+#                                                        })
+# print(response.status_code)
+# print(response.text)
+
+response = requests.get(f'{HOST}/announsments/2')
 print(response.status_code)
 print(response.text)
+
+# response = requests.delete(f'{HOST}/announsments/2')
+# print(response.status_code)
+# print(response.text)
